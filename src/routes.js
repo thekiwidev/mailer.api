48a10a22d4d409b5a2b0e67e5a3609b5e3c083ca
thekiwidev <adedotungabriel20@gmail.com>
2025-01-29 04:26:43 +0100

    sending attachments

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,17 @@ const express = require("express");
 const {
   travelApplicationMailer,
   studentApplicationMailer,
+  handleUpload,
 } = require("./mailer");
 
 const router = express.Router();
 
 // Define your routes here
-router.post("/mailer/travel-application", travelApplicationMailer);
+router.post(
+  "/mailer/travel-application",
+  handleUpload,
+  travelApplicationMailer
+);
 router.post("/mailer/student-application", studentApplicationMailer);
 
 module.exports = router;
